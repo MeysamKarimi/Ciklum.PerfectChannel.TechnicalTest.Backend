cc16b19ca83ac84c42918894144c564e2606d202
Meysam Karimi <Meysam.Karimi84@gmail.com>
2021-05-13 10:13:04 +0200

    User Story 2, User Story 3 include APIs and Unit tests

---
+++ PerfectChannel.WebApi/Common/ResponseCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerfectChannel.WebApi.Common
+{
+    public enum ResponseCode
+    {
+        Success = 200,
+        ErrorServer = 300,
+        ErrorValidation = 400,
+        TimeOut = 500
+    }
+}
